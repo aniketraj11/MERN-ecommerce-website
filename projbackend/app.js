20d0e19d11d780be03d6cde90923e9dfd082a383
aniketraj11 <aniketraj11@gmail.com>
2020-04-12 19:46:30 +0530

    backend module

---
+++ projbackend/app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+var mongoose = require("mongoose");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+//my routes
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
+const categoryRoute = require("./routes/category");
+const productRoute = require("./routes/product");
+
+//DB connection
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNECTED");
+  });
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+
+//PORT
+const port = process.env.PORT || 8000;
+
+//Routes
+app.use("/api", authRoute);
+app.use("/api", userRoute);
+app.use("/api", categoryRoute);
+app.use("/api", productRoute);
+
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
---
+++ projbackend/controllers/category.js
@@ -0,0 +1,67 @@
+const Category = require("../models/category");
+
+exports.getCategoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, cate) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Category not found",
+      });
+    }
+    req.category = cate;
+    next();
+  });
+};
+
+exports.createCategory = (req, res) => {
+  const category = new Category(req.body);
+  category.save((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Error creating category ",
+      });
+    }
+    res.json({ category });
+  });
+};
+
+exports.getCategory = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.getAllCategory = (req, res) => {
+  Category.find().exec((err, categories) => {
+    if (err) {
+      return res.status(400).json({
+        error: "No categories found ",
+      });
+    }
+    res.json({ categories });
+  });
+};
+
+exports.updateCategory = (req, res) => {
+  const category = req.category;
+  category.name = req.body.name;
+  category.save((err, updatedCategory) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Error updating category ",
+      });
+    }
+    res.json({ updatedCategory });
+  });
+};
+
+exports.removeCategory = (req, res) => {
+  const category = req.category;
+  category.remove((err, category) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Error deleteing category ",
+      });
+    }
+    res.json({
+      message: `deleted successfully ${category}`,
+    });
+  });
+};
