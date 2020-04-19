d66478ab38ce71ba77ac097521a1260d36ad6994
aniketraj11 <aniketraj11@gmail.com>
2020-04-19 23:42:30 +0530

    checkout cart component added

--- projfrontend/src/core/Card.js
+++ projfrontend/src/core/Card.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ImageHelper from "./helper/ImageHelper";
+import { addItemToCart, removeItemFromCart } from "./helper/cartHelper";
+import { Redirect } from "react-router-dom";
 
-const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
+const Card = ({
+  product,
+  addtoCart = true,
+  removeFromCart = false,
+  setReload = (f) => f,
+  reload = undefined,
+}) => {
+  const [redirect, setRedirect] = useState(false);
+  const [count, setCount] = useState(product.count);
+
   const cartTitle = product ? product.name : "A photo from pexels";
   const cartDescription = product ? product.description : "Default description";
   const cartPrice = product ? product.price : "Default price";
+
+  const addToCart = () => {
+    addItemToCart(product, () => setRedirect(true));
+  };
+
+  const getARedirect = (redirect) => {
+    if (redirect) {
+      return <Redirect to="/cart" />;
+    }
+  };
+
   const showAddToCart = (addtoCart) => {
     return (
       addtoCart && (
         <button
-          onClick={() => {}}
+          onClick={addToCart}
           className="btn btn-block btn-outline-success mt-2 mb-2"
         >
           Add to Cart
@@ -21,7 +43,10 @@ const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
     return (
       removeFromCart && (
         <button
-          onClick={() => {}}
+          onClick={() => {
+            removeItemFromCart(product._id);
+            setReload(!reload);
+          }}
           className="btn btn-block btn-outline-danger mt-2 mb-2"
         >
           Remove from cart
@@ -33,6 +58,7 @@ const Card = ({ product, addtoCart = true, removeFromCart = false }) => {
     <div className="card text-white bg-dark border border-info ">
       <div className="card-header lead">{cartTitle}</div>
       <div className="card-body">
+        {getARedirect(redirect)}
         <ImageHelper product={product} />
         <p className="lead bg-success font-weight-normal text-wrap">
           {cartDescription}
---
+++ projfrontend/src/core/StripeCheckout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StripeCheckout = () => {
+  return (
+    <div>
+      <h3 className="text-white">Stripe checkout</h3>
+    </div>
+  );
+};
+
+export default StripeCheckout;
